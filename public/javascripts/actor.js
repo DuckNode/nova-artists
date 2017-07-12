39224b699ffc2b7da08864b30148a80a7beddd40
DuckNode <davidj_stewart@yahoo.com>
2017-07-13 00:19:08 +0100

    add footer to layout.jade

--- public/javascripts/actor.js
+++ public/javascripts/actor.js
@@ -1,19 +1,19 @@
 'use strict';
 
 $(document).ready(function () {
-    $('img').addClass('img-responsive');
-    $('#filmTable').DataTable({
-        paging: false,
-        searching: false,
-        info: false,
-        ordering: false,
-        responsive: true
-    });
-    $('#theatreTable').DataTable({
-        paging: false,
-        searching: false,
-        info: false,
-        ordering: false,
-        responsive: true
-    });
+    $('#gallery img').addClass('img-responsive');
+    // $('#filmTable').DataTable({
+    //     paging: false,
+    //     searching: false,
+    //     info: false,
+    //     ordering: false,
+    //     responsive: true
+    // });
+    // $('#theatreTable').DataTable({
+    //     paging: false,
+    //     searching: false,
+    //     info: false,
+    //     ordering: false,
+    //     responsive: true
+    // });
 });
