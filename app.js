25903234b9f40504b9d99aecf9bd7c7a94e331ea
DuckNode <davidj_stewart@yahoo.com>
2017-07-22 21:19:20 +0100

    fix socialmedia html and styles

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var path = require('path');
 var favicon = require('serve-favicon');
+var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -10,12 +10,12 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon-96x96.png')));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
