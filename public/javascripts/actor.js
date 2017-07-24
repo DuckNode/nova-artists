98fcadece3dae39cffccab5ad808e40c1d2b6e8b
DuckNode <davidj_stewart@yahoo.com>
2017-07-24 01:00:33 +0100

    update site responsiveness

--- public/javascripts/actor.js
+++ public/javascripts/actor.js
@@ -2,13 +2,12 @@
 
 $(document).ready(function () {
     $('#gallery img').addClass('img-responsive');
-    // $('#filmTable').DataTable({
-    //     paging: false,
-    //     searching: false,
-    //     info: false,
-    //     ordering: false,
-    //     responsive: true
-    // });
+    $('#filmTable').DataTable({
+        paging: false,
+        searching: false,
+        info: false,
+        ordering: false
+    });
     // $('#theatreTable').DataTable({
     //     paging: false,
     //     searching: false,
